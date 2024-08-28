8931b9a071c6d42e8a477559b798c2f21fd141f8
Ignacio <progremb@gmail.com>
2024-08-27 23:31:19 -0500

    Le cambie el nombre y borre comentarios y cambie poco codigo

--- Taller/Taller/Rectangulo.java
+++ Taller/Taller/Rectangulo.java
@@ -1,38 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package taller;
+package Taller;
 
-
-/**
- *
- * @author User
- */
-public class rectangulo {
-    
+public class Rectangulo 
+{    
     float longitud;
     float anchoRectangulo;
     
-    public rectangulo (float longitud, float anchoRectangulo){
+    public Rectangulo (float longitud, float anchoRectangulo)
+    {
         this.longitud = longitud;   
         this.anchoRectangulo = anchoRectangulo; 
     }    
     
-    public float calcularArea (){
+    public float calcularArea ()
+    {
         return longitud * anchoRectangulo;
     }
     
-    public float calcularPerimetro (){
+    public float calcularPerimetro ()
+    {
         return longitud + longitud + anchoRectangulo + anchoRectangulo;
     }
-    rectangulo(){
+
+    Rectangulo()
+    {
         longitud = 1.0f;
         anchoRectangulo = 2.0f;
     }
   
-    public static void main (String [] args){
-        rectangulo area = new rectangulo();
+    public static void main (String [] args)
+    {
+        Rectangulo area = new Rectangulo();
         System.out.println("el area del rectangulo es de: "+area.calcularArea());
         System.out.println("el perimetro del rectangulo es de: "+area.calcularPerimetro());
     }
