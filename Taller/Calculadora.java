d166e798723445b6eb63f54f5aa636a3f9a81e75
Ignacio <progremb@gmail.com>
2024-08-29 00:42:46 -0500

    cree funciones para cada operacion basica

--- Taller/Calculadora.java
+++ Taller/Calculadora.java
@@ -4,15 +4,43 @@ import java.util.Scanner;
 
 public class Calculadora 
 {
-    static int numero1 = 0;
-    static int numero2 = 0;
-    static float resultado = 0;
+    static int numero1;
+    static int numero2;
+    float resultado;
     
     public Calculadora (int numero1,int numero2,float resultado)
     {
         this.numero1 = numero1;
         this.numero2 = numero2;
         this.resultado = resultado;
+    }
+
+    public float  sumar(int numero1,int numero2)
+    {
+        resultado = numero1 + numero2;
+        System.out.println("El resultado de la suma de "+numero1+" y numero "+numero2+" es: "+ resultado);
+        return resultado;
+    }
+
+    public float restar(int numero1,int numero2)
+    {
+        resultado = numero1 - numero2;
+        System.out.println("El resultado de la resta de "+numero1+" y numero "+numero2+" es: "+ resultado);
+        return resultado;
+    }
+
+    public float multiplicar(int numero1,int numero2)
+    {
+        resultado = numero1 * numero2;
+        System.out.println("El resultado de la multiplicacion de "+numero1+" y numero "+numero2+" es: "+ resultado);
+        return resultado;
+    }
+
+    public float dividir(int numero1,int numero2)
+    {
+        resultado = numero1 / numero2;
+        System.out.println("El resultado de la division de "+numero1+" y numero "+numero2+" es: "+ resultado);
+        return resultado;
     }
 
     public static void main(String[] args)
@@ -24,19 +52,9 @@ public class Calculadora
         System.out.println("Ingresa primer numero: ");
         Scanner num2 = new Scanner(System.in);
         numero2 = num2.nextInt();
+
+        //Calculadora FuncionSuma = new Calculadora(); FuncionSuma.sumar(int numero1;int numero2);
         
-        resultado = numero1 + numero2;
-        System.out.println("El resultado de la suma de "+numero1+" y numero "+numero2+" es: "+ resultado);
-        
-        resultado = numero1 - numero2;
-        System.out.println("El resultado de la resta de "+numero1+" y numero "+numero2+" es: "+ resultado);
-        
-        resultado = numero1 * numero2;
-        System.out.println("El resultado de la multiplicacion de "+numero1+" y numero "+numero2+" es: "+ resultado);
-        
-        resultado = numero1 / numero2;
-        System.out.println("El resultado de la division de "+numero1+" y numero "+numero2+" es: "+ resultado);
-        
-        //System.out.println("hola mundo");
+        System.out.println("cambio hecho 2");
     }   
 }
