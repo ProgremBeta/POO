2950b796b301f1deb276517cfaeb383cc351de80
ProgremBeta <151699660+ProgremBeta@users.noreply.github.com>
2024-08-30 01:32:49 -0500

    Update Rectangulo.java

--- Taller/Rectangulo.java
+++ Taller/Rectangulo.java
@@ -1,9 +1,10 @@
-package Taller;
+//package Taller;
+import java.util.Scanner;
 
 public class Rectangulo 
 {    
-    float longitud;
-    float anchoRectangulo;
+    static float longitud;
+    static float anchoRectangulo;
     
     public Rectangulo (float longitud, float anchoRectangulo)
     {
@@ -11,26 +12,27 @@ public class Rectangulo
         this.anchoRectangulo = anchoRectangulo; 
     }    
     
-    public float calcularArea ()
+    public void calcularArea (float longitud, float anchoRactangulo)
     {
-        return longitud * anchoRectangulo;
+        float resultado = longitud * anchoRectangulo;
+        System.out.println("el area del rectangulo es: "+ resultado);
     }
     
-    public float calcularPerimetro ()
+    public void calcularPerimetro (float longitud,float anchoRectangulo)
     {
-        return longitud + longitud + anchoRectangulo + anchoRectangulo;
-    }
-
-    Rectangulo()
-    {
-        longitud = 1.0f;
-        anchoRectangulo = 2.0f;
+        float resultado =  (2*(longitud))+(2*(anchoRectangulo));
+        System.out.println("el perimetro del rectangulo es: "+ resultado);
     }
   
     public static void main (String [] args)
     {
-        Rectangulo area = new Rectangulo();
-        System.out.println("el area del rectangulo es de: "+area.calcularArea());
-        System.out.println("el perimetro del rectangulo es de: "+area.calcularPerimetro());
+        Scanner Area = new Scanner(System.in);
+        longitud = Area.nextFloat();
+
+        Scanner Perimetro = new Scanner(System.in);
+        anchoRectangulo = Perimetro.nextFloat();
+
+        calcularArea(longitud,anchoRectangulo);
+        calcularPerimetro(longitud,anchoRectangulo);
     }
 }
