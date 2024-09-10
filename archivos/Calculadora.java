9f2670a85b151e769f88cef1ad3929f9bbf0fff8
ProgremBeta <151699660+ProgremBeta@users.noreply.github.com>
2024-09-09 20:19:33 -0500

    Update Calculadora.java
    codigo stiven

--- archivos/Calculadora.java
+++ archivos/Calculadora.java
@@ -1,39 +1,59 @@
-package calculadora;
 import java.util.Scanner;
-/**
- *
- * @author User
- */
-public class Calculadora {
 
-    public int a;
-    public int b;
-    public int resultado=0;
-    
-    public Calculadora (int a, int b)
+public class Calculadora 
+{
+    static float numero1;
+    static float numero2;
+    static float resultado;
+
+    public Calculadora (float numero1,float numero2,float resultado)
     {
-        this.a = a;
-        this.b = b;
+        this.numero1 = numero1;
+        this.numero2 = numero2;
+        this.resultado = resultado;
     }
-    
-    public static void Multiplicacion(int a, int b, int resultado)
+
+    public static  void sumar(float numero1,float numero2)
     {
-        resultado=0;
-        for(int i=0; i>b; i++)
-        {
-            resultado= resultado+a;
-        }
+        resultado = numero1 + numero2;
+        System.out.println("El resultado de la suma de "+numero1+" y numero "+numero2+" es: "+ resultado);
+        //return resultado;
     }
-    
-    //public static void Division(int a, int b)
-    //{
-    //    resultado= 
-    //}
-            
-    public static void main(String[] args) {
+
+    public static void restar(float numero1,float numero2)
+    {
+        resultado = numero1 - numero2;
+        System.out.println("El resultado de la resta de "+numero1+" y numero "+numero2+" es: "+ resultado);
+        //return resultado;
+    }
+
+    public static void multiplicar(float numero1,float numero2)
+    {
+        resultado = numero1 * numero2;
+        System.out.println("El resultado de la multiplicacion de "+numero1+" y numero "+numero2+" es: "+ resultado);
+        //return resultado;
+    }
+
+    public static void dividir(float numero1,float numero2)
+    {
+        resultado = numero1 / numero2;
+        System.out.println("El resultado de la division de "+numero1+" y numero "+numero2+" es: "+ resultado);
+        //return resultado;
+    }
+
+    public static void main(String[] args)
+    {
         System.out.println("Ingresa primer numero: ");
         Scanner num1 = new Scanner(System.in);
-        a = num1.nextInt();
-    }
-    
+        numero1 = num1.nextInt();
+
+        System.out.println("Ingresa primer numero: ");
+        Scanner num2 = new Scanner(System.in);
+        numero2 = num2.nextInt();
+
+        sumar( numero1, numero2);
+        restar( numero1, numero2);
+        multiplicar(numero1,numero2);
+        dividir(numero1,numero2);
+    }   
 }
