78e6dfafb15440d5706377c0dee2aa00c03bebcd
StivenVelez2 <estivenvelez90@gmail.com>
2024-09-25 17:40:18 -0500

    Update Main.java

--- GestionVehiculos/Main.java
+++ GestionVehiculos/Main.java
@@ -1,17 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package gestionvehiculos;
-
+import java.util.Scanner;
+ 
 /**
  *
  * @author User
  */
-public class Main {
+public class Main{
     public static void main(String [] args){
-        Pruebas_vehiculo pruebas_vehiculo = new Pruebas_vehiculo("audi", 20000, 001);
-        pruebas_vehiculo.mostarInformacion();
+        System.out.println("Ingrese la marca del auto: ");
+        Scanner modelo1 = new Scanner (System.in);
+        String modelo = modelo1.nextLine();
+        System.out.println("Ingrese el precio de compra del auto: ");
+        Scanner compra1 = new Scanner (System.in);
+        int precioCompra = compra1.nextInt();
+        System.out.println("Ingrese el ID del auto: ");
+        Scanner ID1 = new Scanner (System.in);
+        int id = ID1.nextInt();
+        System.out.println("Ingrese los años del auto: ");
+        Scanner annosuso1 = new Scanner (System.in);
+        int annosUso = annosuso1.nextInt();
+       
+        Pruebas_vehiculo pruebas_vehiculo = new Pruebas_vehiculo(modelo, precioCompra, id, annosUso);
+        pruebas_vehiculo.mostrarInformacion();
     }
     
+   
 }
+
